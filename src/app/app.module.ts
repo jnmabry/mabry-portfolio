80586f700231a96f227f4ea8a92a93ce1f16b016
Joshua Mabry <inkandcapture@gmail.com>
2017-04-18 11:56:41 -0400

    Installs bootstrap and font-awesome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { routing } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TopNavComponent } from './home/top-nav/top-nav.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TopNavComponent,
+    FooterComponent,
+    PortfolioComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,16 @@
 import { Routes, RouterModule } from "@angular/router";
 import { ModuleWithProviders }  from '@angular/core';
 
-const APP_ROUTES: Routes = [
+// Main Site Pages
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
+const APP_ROUTES: Routes = [
+  { path: '', component: AboutComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'contact', component: ContactComponent},
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
