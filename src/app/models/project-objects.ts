d98de7b81e2ad02c3cbf09f7583c50d6d5e3e024
Joshua Mabry <inkandcapture@gmail.com>
2017-04-24 16:30:40 -0400

    Creates mockups for hug hikeit and weatherly

--- src/app/models/project-objects.ts
+++ src/app/models/project-objects.ts
@@ -3,17 +3,17 @@ import { Project } from '../models/project';
 export const PROJECTS: Project[] = [
   { name: 'HUG - Help Us Grow',
     description: 'Angular 2, TypeScript, JavaScript',
-    image: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97350&w=350&h=350',
+    image: '/assets/hug-opt.jpg',
     url: 'hug-detail'
   },
   { name: 'HikeIt',
     description: 'Android, Java',
-    image: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97350&w=350&h=350',
+    image: '/assets/hikeit-opt.jpg',
     url: 'hikeit-detail'
   },
   { name: 'Weather.ly / Mapl.ly',
     description: 'JavaScript, jQuery, AJAX',
-    image: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97350&w=350&h=350',
+    image: '/assets/weatherly-opt.jpg',
     url: 'weatherly-maply-detail'
   },
   { name: 'Project 4',
