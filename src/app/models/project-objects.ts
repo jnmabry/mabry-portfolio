8a69e27afe370ff313f25c01b0283c5a5a96250b
Joshua Mabry <inkandcapture@gmail.com>
2017-04-25 11:10:32 -0400

    Added tekesports to portfolio page

--- src/app/models/project-objects.ts
+++ src/app/models/project-objects.ts
@@ -6,29 +6,19 @@ export const PROJECTS: Project[] = [
     image: '/assets/hug-opt.jpg',
     url: 'hug-detail'
   },
-  { name: 'HikeIt',
-    description: 'Android, Java',
-    image: '/assets/hikeit-opt.jpg',
-    url: 'hikeit-detail'
-  },
-  { name: 'Weather.ly / Mapl.ly',
+    { name: 'Weather.ly / Mapl.ly',
     description: 'JavaScript, jQuery, AJAX',
     image: '/assets/weatherly-opt.jpg',
     url: 'weatherly-maply-detail'
   },
-  { name: 'Project 4',
-    description: 'This was a cool project',
-    image: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97350&w=350&h=350',
+    { name: 'TekEsports',
+    description: 'UX/UI Design, Android, Java',
+    image: '/assets/tekesports-opt.jpg',
     url: 'hug'
   },
-  { name: 'Project 5',
-    description: 'This was a cool project',
-    image: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97350&w=350&h=350',
-    url: 'hug'
-  },
-  { name: 'Project 6',
-    description: 'This was a cool project',
-    image: 'https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97350&w=350&h=350',
-    url: 'hug'
+  { name: 'HikeIt',
+    description: 'UX/UI Design, Android, Java',
+    image: '/assets/hikeit-opt.jpg',
+    url: 'hikeit-detail'
   },
 ];
